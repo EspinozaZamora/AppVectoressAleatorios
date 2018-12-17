8e92329a2b7c376a910d15d7bd171f70fd86d706
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-12-17 08:59:47 -0500

    Vectores Aleatorios

--- AppVectoressAleatorios/Program.cs
+++ AppVectoressAleatorios/Program.cs
@@ -8,8 +8,31 @@ namespace AppVectoressAleatorios
 {
     class Program
     {
+        const int MAX = 100;
+
         static void Main(string[] args)
         {
+            int[] x = new int[MAX];
+            leerVectorr(x);
+            Console.WriteLine("\n Elementosdel Vector  ");
+            imprimirVector(x);
+            Console.ReadKey();
+
+        }
+        public static void leerVectorr (int [] x)
+        {
+            for(int i=0; i<MAX; i++)
+            {
+                var randomNumber = new Random().Next(1, 5000);
+                x[i] = randomNumber;
+
+
+            }
+        }
+        public static void imprimirVector(int [] x)
+        {
+            for (int i = 0; i < MAX; i++)
+                Console.WriteLine(x[i]);
         }
     }
 }
